69ecce8a798fd707504a5569760f8fcc816bc402
Hammad <hammad@emumba.com>
2024-08-18 14:48:55 +0500

    Parse the data to new file according to the specified format. Combine mean_fermi values in a single section

--- test.py
+++ test.py
@@ -1,3 +1,106 @@
+# def merge_sect(parsed_groups):
+#     result_dict = {
+#         'group1': [],
+#         'group2': [],
+#         'group3': [],
+#         'group4': []
+#     }
+    
+#     group1 = ['gpu', 'machine_name', 'design_size']
+#     group2 = ['high_curvature_internal_checking_count', 'mrc_area_count']
+#     group3 = ['mean_fermi']
+#     group4 = ['target_prep_runtime']
+
+#     for item in parsed_groups:
+#         lines = item.split('\n')
+#         first_line = lines[0]
+#         key = first_line.split(' = ', 1)[0]
+#         remaining_lines = '\n'.join(lines[1:])
+        
+#         if key in group1:
+#             result_dict['group1'].append(first_line + '\n' + remaining_lines)
+#         elif key in group2:
+#             result_dict['group2'].append(first_line + '\n' + remaining_lines)
+#         elif key in group3:
+#             result_dict['group3'].append(first_line + '\n' + remaining_lines)
+#         elif key in group4:
+#             result_dict['group4'].append(first_line + '\n' + remaining_lines)
+    
+#     output_list = []
+#     for group in ['group1', 'group2', 'group3', 'group4']:
+#         if result_dict[group]:
+#             output_list.append('\n'.join(result_dict[group]))
+
+#     output_list[1], output_list[3] = output_list[3], output_list[1]
+#     output_list[2], output_list[3] = output_list[3], output_list[2]
+
+#     return output_list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def merge_sect(parsed_groups):
+#     result_dict = {}
+
+#     for item in parsed_groups:
+#         lines = item.split('\n')
+#         first_line = lines[0]
+#         key = first_line.split(' = ', 1)[0]
+#         remaining_lines = '\n'.join(lines[1:])
+        
+#         if key in result_dict:
+#             # If the key exists, append the remaining lines
+#             result_dict[key] += '\n' + first_line + '\n' + remaining_lines
+#         else:
+#             # If the key doesn't exist, create a new entry using the key
+#             result_dict[key] = first_line + '\n' + remaining_lines
+    
+#     output_list = []
+#     for _, value in result_dict.items():
+#         output_list.append(f"{value}")
+
+    # return output_list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # import os
 # def parse_logfile(logfile_path, output_file):
 #     additional_lines = [
@@ -39,7 +142,35 @@
 
 
 
+# import os
+# def parse_logfile(logfile_path, output_file):
+    
+#     with open(logfile_path, 'r') as logfile, open(output_file, "w") as file:
+#         for line in logfile:
+#             stripped_line = line.strip()
+#             if stripped_line == "":
+#                 file.write("\n")
+                
+#             elif '=' in stripped_line:
+#                 key, value = stripped_line.split('=', 1)
+#                 key, value = key.strip(), value.strip()
+#                 file.write(f"{key} = {value}\n")
 
+#         print(f"Data extracted from input file {logfile_path}")
+#         print(f"Data written to the output file {output_file}")
+    
+# def main():
+#     logfile_path = "dummy_logfile.txt"
+#     if os.path.exists(logfile_path):
+#         print(f"The input file namely {logfile_path} exists")
+#     else:
+#         print("File does not exist")
+#     output_file = "generated.txt"
+#     parse_logfile(logfile_path, output_file)
+#     print("Parsing complete")
+
+# if __name__ == "__main__":
+#     main()
 
 
 
